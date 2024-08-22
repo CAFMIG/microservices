7311c19e8b446ef80ced5fae7843e08d50b8ef52
Miguel Cafruni <4996097@defpub.local>
2024-08-22 18:46:04 -0300

    Tolerância a falhas com Hystrix.

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,19 +2,19 @@ package com.devsuperior.hrworker.resources;
 
 import java.util.List;
 
-import org.springframework.core.env.Environment;
+import java.lang.Thread;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -35,6 +35,15 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+/*         try {
+            Thread.sleep(3000L);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        } */
+ 
+
         logger.info("PORT = " + env.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
